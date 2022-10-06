8195e7b1dd284133cf04b0655c560923a45a05f7
毛线球上的喵星人 <785315332@qq.com>
2022-10-06 23:06:45 +0800

    添加网站框架文件

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    startUpdateTime()
+}
+
+
+function timestampToTime(timestamp) {
+    var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+    var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+    var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+    var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+    return Y + M + D + h + m + s;
+}
+
+function startUpdateTime() {
+    var TIME_UPDATER_ID = setInterval(() => {
+        let nowTime = timestampToTime(new Date().getTime())
+        document.getElementById("nowTime").innerHTML = nowTime
+    }, 500)
+}
